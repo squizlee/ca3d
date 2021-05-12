7cff89ec9df51e1efb5b93a8f544b525a9823c05
Squizlee <ethanscavia777@gmail.com>
2021-05-13 00:08:21 +1000

    custom vertex buffer for a single triangle render

--- public/scripts/run.js
+++ public/scripts/run.js
@@ -1,16 +1,9 @@
+// boiler plate set up
 setScene();
+// create grid and add grid to scene
+//createGrid(10, 10, 10, 1);
+//addShapes();
+createBufferCube();
 
-// Radius min, max, hlines and vlines min, max, equal hvlines
-//paramSphere(1, 2, 1, 20, true, true);
-
-createGrid(10, 10, 10, 1);
-
-// Creates rings, numRings, isMobius
-//createRings(6);
-addShapes();
-
-//animate_sphere();
 animate();
-//renderer.render(scene, camera);
-
-window.addEventListener('resize', resizeScene);
+window.addEventListener("resize", resizeScene);
--- public/scripts/setup.js
+++ public/scripts/setup.js
@@ -1,31 +1,60 @@
 /* global THREE */
-//Declare Systen Variables
+//Declare Systen Variables and boilerplate
 
-var scene;
-var camera;
-var renderer;
-var controls;
+let scene;
+let camera;
+let renderer;
+let controls;
+let raycaster;
 
 //Setup the 3 main components: scene, camera, renderer
 function setScene() {
-    scene = new THREE.Scene();
-    var ratio = window.innerWidth / window.innerHeight;
-    camera = new THREE.PerspectiveCamera(45, ratio, 0.1, 1000);
-    camera.position.set(100, 0, 0);
-    camera.lookAt(0, 5, 0);
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-    controls = new THREE.OrbitControls(camera, renderer.domElement);
+	raycaster = new THREE.Raycaster();
+
+	scene = new THREE.Scene();
+	var ratio = window.innerWidth / window.innerHeight;
+	camera = new THREE.PerspectiveCamera(45, ratio, 0.1, 1000);
+	//camera.lookAt(0, 5, 0);
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
+	controls = new THREE.OrbitControls(camera, renderer.domElement);
+    camera.position.set(0.0,0.0,20);
+    console.log("Check out the position", camera);
 }
 
 //Resize the scene and update the camera aspect to the screen ration
-var resizeScene = function() {
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    renderer.setSize(width, height);
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
+var resizeScene = function () {
+	var width = window.innerWidth;
+	var height = window.innerHeight;
+	renderer.setSize(width, height);
+	camera.aspect = width / height;
+	camera.updateProjectionMatrix();
 
-    renderer.render(scene, camera);
+	renderer.render(scene, camera);
 };
+
+function createBufferCube() {
+	// let's just worry about positioning of the vertices
+	const geometry = new THREE.BufferGeometry();
+
+    // this just generates a single triangle for now
+    const vertices = new Float32Array( [
+        // front 
+        -1, 1, 1,
+        1, 1, 1,
+        -1, -1, 1,
+    ]);
+    //BUG: The positioning of the mesh and camera are weird like wtf????
+
+    // TODO: Index redundant vertices to reduce memory load
+
+    geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
+    const material = new THREE.MeshBasicMaterial({
+        color: 0x00ff00
+    });
+    const mesh = new THREE.Mesh(geometry, material);
+    console.log(mesh);
+    scene.add(mesh);
+    mesh.position.set(0, 0, 10);
+}
