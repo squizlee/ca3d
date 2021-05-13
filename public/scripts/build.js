f8ac1e63d3a3a70c591adfd88a2eb9676b9bc9be
Sam <sam.huang852@gmail.com>
2021-05-14 00:13:20 +1000

    lighting

--- public/scripts/build.js
+++ public/scripts/build.js
@@ -1,4 +1,6 @@
 /* global THREE, scene, renderer, camera */
+var ambientlight;
+var cameralight;
 
 function createCube(r, color){
     var material = new THREE.MeshBasicMaterial();
@@ -7,6 +9,17 @@ function createCube(r, color){
     var geometry_cube = new THREE.BoxGeometry(r, r, r);
     var cube = new THREE.Mesh(geometry_cube, material);
     return cube;
+}
+
+function addLight() {
+    cameralight = new THREE.PointLight(new THREE.Color(1, 1, 1), 0.3);
+    cameralight.castShadow = true;
+    ambientlight = new THREE.AmbientLight(new THREE.Color(1, 1, 1), 1);
+    camera.add(cameralight);
+
+    scene.add(ambientlight);
+    //var spotLightHelper = new THREE.SpotLightHelper( spotlight );
+    //scene.add( spotLightHelper );
 }
 
 function addShapes() {
